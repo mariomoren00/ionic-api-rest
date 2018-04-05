1a082050c5b3a9b5f6224a2fb973ce3927cbc43d
Mario Moreno <mario.moreno.a93@gmail.com>
2018-04-04 21:34:10 -0500

    Add comments
    Comentarios a los métodos

--- categoryapp/src/providers/categories/categories.ts
+++ categoryapp/src/providers/categories/categories.ts
@@ -14,28 +14,33 @@ export class CategoriesProvider {
     console.log('Hello CategoriesProvider Provider');
   }
 
+  //Método que obtiene a todas las categorias
   getCategories(){
     return this.http.get('http://localhost:3000/api/v1/categories');
   }
 
+  //Método que obtiene la categoría por su id
   getCategoryFindById(id){
     console.log('get category');
     console.log(id);
     return this.http.get(`http://localhost:3000/api/v1/categories/${id}`)
   }
 
+  //Método que almacena una categoría
   saveCategory(categories){
     console.log('Save');
     console.log(categories);
     return this.http.post('http://localhost:3000/api/v1/categories',categories)
   }
 
+  //Método que modifica una categoría
   updateCategory(category){
     console.log('get category');
     console.log(category.id);
     return this.http.put(`http://localhost:3000/api/v1/categories/${category.id}`, {'name':category.name});
   }
 
+  //Método que elimina una categoría
   deleteCategory(id){
     console.log('get category id delete');
     console.log(id);
